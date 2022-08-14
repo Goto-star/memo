cab8bb3d00815e9bb77b20455bd3f11260a622e7
EC2 Default User <ec2-user@ip-172-31-4-27.us-west-2.compute.internal>
2022-08-14 03:29:10 +0000

    change memo.rb

--- memo.rb
+++ memo.rb
@@ -2,31 +2,31 @@ require "csv"
 
 puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 
-memo_type = gets.to_s
+memo_type = gets.to_i
 
-if memo_type = 1
+if memo_type == 1
     puts "拡張子を除いたファイルを入力してください"
-    memo_title = gets.to_s
+    memo_title = gets.chomp
     
     puts "メモしたい内容を記入してください"
     puts "完了したらCtrl + Dをおします"
-    memo_content = gets.to_s
+    memo_content = gets.chomp
     
     CSV.open("#{memo_title}.csv", "w") do |csv|
         csv << ["#{memo_content}"]
     end
-elsif memo_type = 2
+elsif memo_type == 2
     puts "編集するファイルを拡張子を除いて入力してください"
-    memo_selected_title = gets.to_s
+    memo_selected_title = gets.chomp
     
     puts "編集内容を記入してください"
-    memo_changed_content = gets.to_s
+    memo_changed_content = gets.chomp
     
-    CSV.open("#{memo_selected_title}.csv", "w") do |csv|
+    CSV.open("#{memo_selected_title}.csv", "a") do |csv|
         csv << ["#{memo_changed_content}"]
     end
 else
-    puts "1か2を選択してください"
+    puts "半角の1か2を選択してください"
 end
     
     
